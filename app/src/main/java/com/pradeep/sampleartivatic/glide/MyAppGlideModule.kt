36a2c2eb8336ba139d9a4833bf37319b91207a80
pradeepsingh <p9201439460@gmail.com>
2022-09-28 16:39:27 +0530

    Show error message for glide in logcat

--- app/src/main/java/com/pradeep/sampleartivatic/glide/MyAppGlideModule.kt
+++ app/src/main/java/com/pradeep/sampleartivatic/glide/MyAppGlideModule.kt
@@ -1,6 +1,7 @@
 package com.pradeep.sampleartivatic.glide
 
 import android.content.Context
+import android.util.Log
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
@@ -8,6 +9,8 @@ import com.bumptech.glide.module.AppGlideModule
 @GlideModule
 class MyAppGlideModule : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
-        super.applyOptions(context, builder)
+        if (builder != null) {
+            builder.setLogLevel(Log.ERROR)
+        }
     }
 }
