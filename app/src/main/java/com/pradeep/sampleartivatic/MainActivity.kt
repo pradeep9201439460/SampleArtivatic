9e9d2f41c7484f0849e9bb21c310f021f7fc5ba2
pradeepsingh <p9201439460@gmail.com>
2022-09-28 14:54:59 +0530

    Enable data binding

--- app/src/main/java/com/pradeep/sampleartivatic/MainActivity.kt
+++ app/src/main/java/com/pradeep/sampleartivatic/MainActivity.kt
@@ -3,18 +3,21 @@ package com.pradeep.sampleartivatic
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.pradeep.sampleartivatic.databinding.ActivityMainBinding
 import com.pradeep.sampleartivatic.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.simpleName
+    lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         viewModel.country.observe(this, {
             Log.d(TAG, it.toString())
